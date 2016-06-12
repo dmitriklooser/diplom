7d308a98864aee23f0f9d157a2fbdf30c0e0fb58
Dmitry <kaynovd@gmail.com>
2016-06-12 11:18:39 +0300

    tabl.sql

--- Diplom/src/main/resources/tabl.sql
+++ Diplom/src/main/resources/tabl.sql
@@ -59,7 +59,8 @@ CONSTRAINT PK_Room PRIMARY KEY (id)
 CREATE TABLE Timeslot (
 id serial,
 lessontime integer NOT NULL,
-day varchar(20) NOT NULL,
+day varchar(20) NOT NULL, 
+inuse boolean DEFAULT false NOT NULL,
 CONSTRAINT PK_Timeslot PRIMARY KEY (id)
 );
 
@@ -76,3 +77,35 @@ CONSTRAINT FK_Lessons_Module FOREIGN KEY (moduleId) REFERENCES Module(id),
 CONSTRAINT FK_Lessons_Professor FOREIGN KEY (professorId) REFERENCES Professor(id),
 CONSTRAINT FK_Lessons_Room FOREIGN KEY (roomId) REFERENCES Room(id)
 );
+
+INSERT INTO Timeslot (lessontime, day) 
+   VALUES (9, 'MONDAY'),
+		(11, 'MONDAY'),
+		(13, 'MONDAY'),
+		(15, 'MONDAY'),
+		(17, 'MONDAY'),
+		(9, 'TUESDAY'),
+		(11, 'TUESDAY'),
+		(13, 'TUESDAY'),
+		(15, 'TUESDAY'),
+		(17, 'TUESDAY'),
+		(9, 'WEDNESDAY'),
+		(11, 'WEDNESDAY'),
+		(13, 'WEDNESDAY'),
+		(15, 'WEDNESDAY'),
+		(17, 'WEDNESDAY'),
+		(9, 'THURSDAY'),
+		(11, 'THURSDAY'),
+		(13, 'THURSDAY'),
+		(15, 'THURSDAY'),
+		(17, 'THURSDAY'),
+		(9, 'FRIDAY'),
+		(11, 'FRIDAY'),
+		(13, 'FRIDAY'),
+		(15, 'FRIDAY'),
+		(17, 'FRIDAY'),
+		(9, 'SATURDAY'),
+		(11, 'SATURDAY'),
+		(13, 'SATURDAY'),
+		(15, 'SATURDAY'),
+		(17, 'SATURDAY');
